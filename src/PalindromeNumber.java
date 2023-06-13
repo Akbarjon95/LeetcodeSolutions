e3bfd54313261c72fa8abac9006839aea1dc3ffc
Akbarjon <82497829+Akbarjon95@users.noreply.github.com>
2023-06-13 09:58:54 +0500

    9 PalindromeNumber

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,22 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int x = 121;
+        isPalindrome(x);
+    }
+
+    public static boolean isPalindrome(int x) {
+        int orginal = x;
+        int sum = 0;
+        int residual;
+        while (x >= 1) {
+            residual = x % 10;
+            sum = sum * 10 + residual;
+            x /= 10;
+        }
+        if (orginal == sum) {
+            return true;
+        } else
+            return false;
+
+    }
+}
